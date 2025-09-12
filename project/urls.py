6669bc0273eab350f135febf6a05e1fc0a1618ae
Abel Soto Valdez <abelsotovaldez@MacBook-Air-de-Abel.local>
2025-09-12 16:45:28 -0600

    feat(project/url): add static files and redirect

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.views.generic import RedirectView
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Redirects
+    path("", RedirectView.as_view(url="/admin/"), name="home-redirect-admin"),
+    path(
+        "accounts/login/",
+        RedirectView.as_view(url="/admin/"),
+        name="login-redirect-admin",
+    ),
 ]
+
+if not settings.STORAGE_AWS:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
