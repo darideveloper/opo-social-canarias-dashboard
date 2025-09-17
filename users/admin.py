f047936a7f807a8f4c15d8a9d0de501c40625729
Abel Soto Valdez <abelsotovaldez@MacBook-Air-de-Abel.local>
2025-09-16 23:10:28 -0600

    feat(users/admin): register models into admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from users import models
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "last_pass",
+        "profile_img",
+        "points",
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(models.TempToken)
+class TempTokenAdmin(admin.ModelAdmin):
+    list_display = ("profile", "token", "type", "is_active", "created_at", "updated_at")
