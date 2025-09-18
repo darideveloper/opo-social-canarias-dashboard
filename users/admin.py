c09a54df1390a54da5f439e23917027bc355df5b
Abel Soto Valdez <abelsotovaldez@MacBook-Air-de-Abel.local>
2025-09-17 18:57:57 -0600

    feat(users/admin): add admin model features

--- users/admin.py
+++ users/admin.py
@@ -13,7 +13,19 @@ class ProfileAdmin(admin.ModelAdmin):
         "updated_at",
     )
 
+    search_fields = ("user__username", "user__email")
+    list_filter = ("created_at", "updated_at")
+    ordering = ("-created_at",)
+    list_per_page = 50
+    date_hierarchy = "created_at"
+
 
 @admin.register(models.TempToken)
 class TempTokenAdmin(admin.ModelAdmin):
     list_display = ("profile", "token", "type", "is_active", "created_at", "updated_at")
+
+    search_fields = ("profile__user__username", "profile__user__email")
+    list_filter = ("type", "is_active", "created_at", "updated_at")
+    ordering = ("-created_at",)
+    list_per_page = 50
+    date_hierarchy = "created_at"
