1bd9d592c55a67480cc24b7f4a74ad82157789ee
generic-projects <abelsotovaldez@gmail.com>
2025-09-19 22:57:23 -0600

    feat(users/views): add register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.serializers import (
     CustomTokenObtainPairSerializer,
@@ -11,3 +15,23 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 class CustomTokenRefreshView(TokenRefreshView):
     serializer_class = CustomTokenRefreshSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            return Response(
+                {
+                    "message": "Account created successfully. Please check your email to activate your account.",
+                    "user": {
+                        "username": user.username,
+                        "email": user.email,
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
